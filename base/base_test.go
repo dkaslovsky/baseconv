778c37074fa9de40b440b79b73aa68b4956d9cf5
dkaslovsky <dkaslovsky@gmail.com>
2021-04-16 20:22:22 -0600

    use uint64 instead of int64

--- base/base_test.go
+++ base/base_test.go
@@ -7,54 +7,54 @@ import (
 
 func TestToBase10(t *testing.T) {
 	type testCase struct {
-		num      []int64
-		base     int64
-		expected int64
+		num      []uint64
+		base     uint64
+		expected uint64
 	}
 
 	tests := map[string]testCase{
 		"convert single zero": {
-			num:      []int64{0},
+			num:      []uint64{0},
 			base:     2,
 			expected: 0,
 		},
 		"convert multiple zeros": {
-			num:      []int64{0, 0, 0, 0, 0, 0},
+			num:      []uint64{0, 0, 0, 0, 0, 0},
 			base:     2,
 			expected: 0,
 		},
 		"convert from binary": {
-			num:      []int64{1, 0, 1},
+			num:      []uint64{1, 0, 1},
 			base:     2,
 			expected: 5,
 		},
 		"convert from binary with leading zeros": {
-			num:      []int64{0, 0, 1, 1},
+			num:      []uint64{0, 0, 1, 1},
 			base:     2,
 			expected: 3,
 		},
 		"convert from binary with trailing zeros": {
-			num:      []int64{1, 1, 0, 0},
+			num:      []uint64{1, 1, 0, 0},
 			base:     2,
 			expected: 12,
 		},
 		"convert from base 62 with number less than 62": {
-			num:      []int64{5},
+			num:      []uint64{5},
 			base:     62,
 			expected: 5,
 		},
 		"convert from base 62 with number equal to 62": {
-			num:      []int64{1, 0},
+			num:      []uint64{1, 0},
 			base:     62,
 			expected: 62,
 		},
 		"convert from base 62 number with number larger than 62": {
-			num:      []int64{61, 60, 14, 45, 17, 10, 24},
+			num:      []uint64{61, 60, 14, 45, 17, 10, 24},
 			base:     62,
 			expected: 3_520_000_000_000,
 		},
 		"convert from base 10 to base 10": {
-			num:      []int64{5, 8, 7},
+			num:      []uint64{5, 8, 7},
 			base:     10,
 			expected: 587,
 		},
@@ -75,25 +75,25 @@ func TestToBase10(t *testing.T) {
 
 func TestToBase10WithError(t *testing.T) {
 	type testCase struct {
-		num  []int64
-		base int64
+		num  []uint64
+		base uint64
 	}
 
 	tests := map[string]testCase{
 		"convert number equal to target base": {
-			num:  []int64{5},
+			num:  []uint64{5},
 			base: 5,
 		},
 		"convert number larger than target base": {
-			num:  []int64{501},
+			num:  []uint64{501},
 			base: 500,
 		},
 		"target base 0": {
-			num:  []int64{1},
+			num:  []uint64{1},
 			base: 0,
 		},
 		"target base 1": {
-			num:  []int64{0},
+			num:  []uint64{0},
 			base: 1,
 		},
 	}
@@ -110,46 +110,46 @@ func TestToBase10WithError(t *testing.T) {
 
 func TestFromBase10(t *testing.T) {
 	type testCase struct {
-		num      int64
-		base     int64
-		expected []int64
+		num      uint64
+		base     uint64
+		expected []uint64
 	}
 
 	tests := map[string]testCase{
 		"convert zero": {
 			num:      0,
 			base:     2,
-			expected: []int64{0},
+			expected: []uint64{0},
 		},
 		"convert to binary": {
 			num:      5,
 			base:     2,
-			expected: []int64{1, 0, 1},
+			expected: []uint64{1, 0, 1},
 		},
 		"convert to binary with trailing zeros": {
 			num:      12,
 			base:     2,
-			expected: []int64{1, 1, 0, 0},
+			expected: []uint64{1, 1, 0, 0},
 		},
 		"convert to base 62 with number less than 62": {
 			num:      5,
 			base:     62,
-			expected: []int64{5},
+			expected: []uint64{5},
 		},
 		"convert to base 62 with number equal to 62": {
 			num:      62,
 			base:     62,
-			expected: []int64{1, 0},
+			expected: []uint64{1, 0},
 		},
 		"convert to base 62 with number larger than 62": {
 			num:      3_520_000_000_000,
 			base:     62,
-			expected: []int64{61, 60, 14, 45, 17, 10, 24},
+			expected: []uint64{61, 60, 14, 45, 17, 10, 24},
 		},
 		"convert to base 10 from base 10": {
 			num:      587,
 			base:     10,
-			expected: []int64{5, 8, 7},
+			expected: []uint64{5, 8, 7},
 		},
 	}
 
@@ -168,8 +168,8 @@ func TestFromBase10(t *testing.T) {
 
 func TestFromBase10WithError(t *testing.T) {
 	type testCase struct {
-		num  int64
-		base int64
+		num  uint64
+		base uint64
 	}
 
 	tests := map[string]testCase{

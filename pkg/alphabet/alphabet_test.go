e85eab5269e73b38020a34018be0aff911631595
dkaslovsky <dkaslovsky@gmail.com>
2022-03-06 12:47:30 -0700

    standardize test messages

--- pkg/alphabet/alphabet_test.go
+++ pkg/alphabet/alphabet_test.go
@@ -1,135 +1,8 @@
 package alphabet
 
 import (
-	"reflect"
 	"testing"
 )
-
-func TestToString(t *testing.T) {
-	type testCase struct {
-		numeric  []uint64
-		expected string
-	}
-
-	tests := map[string]testCase{
-		"single zero": {
-			numeric:  []uint64{0},
-			expected: "0",
-		},
-		"multiple zeros": {
-			numeric:  []uint64{0, 0, 0, 0},
-			expected: "0000",
-		},
-		"single Z": {
-			numeric:  []uint64{61},
-			expected: "Z",
-		},
-		"heLLo123": {
-			numeric:  []uint64{17, 14, 47, 47, 24, 1, 2, 3},
-			expected: "heLLo123",
-		},
-	}
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			val, err := ToString(test.numeric)
-			if err != nil {
-				t.Fatalf("test case \"%s\": non nil error: %s", name, err)
-			}
-			if val != test.expected {
-				t.Fatalf("test case \"%s\": expected %s not equal to actual %s", name, test.expected, val)
-			}
-		})
-	}
-}
-
-func TestToStringWithError(t *testing.T) {
-	type testCase struct {
-		numeric []uint64
-	}
-
-	tests := map[string]testCase{
-		"single value equal to alphabet size": {
-			numeric: []uint64{Len()},
-		},
-		"single value larger than alphabet size": {
-			numeric: []uint64{Len() + 1},
-		},
-		"mixed values of larger than and smaller than alphabet size": {
-			numeric: []uint64{0, Len() + 1},
-		},
-	}
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			_, err := ToString(test.numeric)
-			if err == nil {
-				t.Fatalf("test case \"%s\": expected non nil error", name)
-			}
-		})
-	}
-}
-
-func TestToPaddedString(t *testing.T) {
-	type testCase struct {
-		str      string
-		strLen   int
-		expected string
-	}
-
-	tests := map[string]testCase{
-		"strLen equal to length of input": {
-			str:      "heLLo",
-			strLen:   5,
-			expected: "heLLo",
-		},
-		"strLen greater than length of input": {
-			str:      "heLLo",
-			strLen:   10,
-			expected: "00000heLLo",
-		},
-		"empty string input": {
-			str:      "",
-			strLen:   5,
-			expected: "00000",
-		},
-	}
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			str, err := Pad(test.str, test.strLen)
-			if err != nil {
-				t.Fatalf("test case \"%s\": non nil error: %s", name, err)
-			}
-			if str != test.expected {
-				t.Fatalf("test case \"%s\": expected %s not equal to actual %s", name, test.expected, str)
-			}
-		})
-	}
-}
-
-func TestPadWithError(t *testing.T) {
-	type testCase struct {
-		str    string
-		strLen int
-	}
-
-	tests := map[string]testCase{
-		"strLen less than length of input": {
-			str:    "heLLo",
-			strLen: 2,
-		},
-	}
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			_, err := Pad(test.str, test.strLen)
-			if err == nil {
-				t.Fatalf("test case \"%s\": expected non nil error", name)
-			}
-		})
-	}
-}
 
 func TestFromString(t *testing.T) {
 	type testCase struct {
@@ -161,13 +34,21 @@ func TestFromString(t *testing.T) {
 	}
 
 	for name, test := range tests {
+		test := test
 		t.Run(name, func(t *testing.T) {
-			val, err := FromString(test.str)
+			res, err := FromString(test.str)
 			if err != nil {
-				t.Fatalf("test case \"%s\": non nil error: %s", name, err)
+				t.Fatalf("unexpected non nil error: %v", err)
 			}
-			if !reflect.DeepEqual(val, test.expected) {
-				t.Fatalf("test case \"%s\": expected %v not equal to actual %v", name, test.expected, val)
+			if len(res) != len(test.expected) {
+				t.Errorf("result %v not equal to expected %v", res, test.expected)
+				return
+			}
+			for i := 0; i < len(res); i++ {
+				if res[i] != test.expected[i] {
+					t.Errorf("result %v not equal to expected %v", res, test.expected)
+					return
+				}
 			}
 		})
 	}
@@ -194,10 +75,141 @@ func TestFromStringWithError(t *testing.T) {
 	}
 
 	for name, test := range tests {
+		test := test
 		t.Run(name, func(t *testing.T) {
 			_, err := FromString(test.str)
 			if err == nil {
-				t.Fatalf("test case \"%s\": expected non nil error", name)
+				t.Fatal("expected non nil error")
+			}
+		})
+	}
+}
+
+func TestToString(t *testing.T) {
+	type testCase struct {
+		numeric  []uint64
+		expected string
+	}
+
+	tests := map[string]testCase{
+		"single zero": {
+			numeric:  []uint64{0},
+			expected: "0",
+		},
+		"multiple zeros": {
+			numeric:  []uint64{0, 0, 0, 0},
+			expected: "0000",
+		},
+		"single Z": {
+			numeric:  []uint64{61},
+			expected: "Z",
+		},
+		"heLLo123": {
+			numeric:  []uint64{17, 14, 47, 47, 24, 1, 2, 3},
+			expected: "heLLo123",
+		},
+	}
+
+	for name, test := range tests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			res, err := ToString(test.numeric)
+			if err != nil {
+				t.Fatalf("unexpected non nil error: %v", err)
+			}
+			if res != test.expected {
+				t.Errorf("result %s not equal to expected %s", res, test.expected)
+			}
+		})
+	}
+}
+
+func TestToStringWithError(t *testing.T) {
+	type testCase struct {
+		numeric []uint64
+	}
+
+	tests := map[string]testCase{
+		"single value equal to alphabet size": {
+			numeric: []uint64{Len()},
+		},
+		"single value larger than alphabet size": {
+			numeric: []uint64{Len() + 1},
+		},
+		"mixed values of larger than and smaller than alphabet size": {
+			numeric: []uint64{0, Len() + 1},
+		},
+	}
+
+	for name, test := range tests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			_, err := ToString(test.numeric)
+			if err == nil {
+				t.Fatal("expected non nil error")
+			}
+		})
+	}
+}
+
+func TestPad(t *testing.T) {
+	type testCase struct {
+		str      string
+		strLen   int
+		expected string
+	}
+
+	tests := map[string]testCase{
+		"strLen equal to length of input": {
+			str:      "heLLo",
+			strLen:   5,
+			expected: "heLLo",
+		},
+		"strLen greater than length of input": {
+			str:      "heLLo",
+			strLen:   10,
+			expected: "00000heLLo",
+		},
+		"empty string input": {
+			str:      "",
+			strLen:   5,
+			expected: "00000",
+		},
+	}
+
+	for name, test := range tests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			res, err := Pad(test.str, test.strLen)
+			if err != nil {
+				t.Fatalf("unexpected non nil error: %v", err)
+			}
+			if res != test.expected {
+				t.Errorf("result %s not equal to expected %s", res, test.expected)
+			}
+		})
+	}
+}
+
+func TestPadWithError(t *testing.T) {
+	type testCase struct {
+		str    string
+		strLen int
+	}
+
+	tests := map[string]testCase{
+		"strLen less than length of input": {
+			str:    "heLLo",
+			strLen: 2,
+		},
+	}
+
+	for name, test := range tests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			_, err := Pad(test.str, test.strLen)
+			if err == nil {
+				t.Fatal("expected non nil error")
 			}
 		})
 	}
--- pkg/baseconv/baseconv_test.go
+++ pkg/baseconv/baseconv_test.go
@@ -2,9 +2,108 @@ package baseconv
 
 import (
 	"math"
-	"reflect"
 	"testing"
 )
+
+func TestFromBase10(t *testing.T) {
+	type testCase struct {
+		num      uint64
+		base     uint64
+		expected []uint64
+	}
+
+	tests := map[string]testCase{
+		"convert zero": {
+			num:      0,
+			base:     2,
+			expected: []uint64{0},
+		},
+		"convert to binary": {
+			num:      5,
+			base:     2,
+			expected: []uint64{1, 0, 1},
+		},
+		"convert to binary with trailing zeros": {
+			num:      12,
+			base:     2,
+			expected: []uint64{1, 1, 0, 0},
+		},
+		"convert to base 62 with number less than 62": {
+			num:      5,
+			base:     62,
+			expected: []uint64{5},
+		},
+		"convert to base 62 with number equal to 62": {
+			num:      62,
+			base:     62,
+			expected: []uint64{1, 0},
+		},
+		"convert to base 62 with number larger than 62": {
+			num:      3_520_000_000_000,
+			base:     62,
+			expected: []uint64{61, 60, 14, 45, 17, 10, 24},
+		},
+		"convert to base 10 from base 10": {
+			num:      587,
+			base:     10,
+			expected: []uint64{5, 8, 7},
+		},
+		"convert to base 10 from base 10 with multiple of 10": {
+			num:      1000,
+			base:     10,
+			expected: []uint64{1, 0, 0, 0},
+		},
+	}
+
+	for name, test := range tests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			res, err := FromBase10(test.num, test.base)
+			if err != nil {
+				t.Fatalf("unexpected non nil error: %v", err)
+			}
+			if len(res) != len(test.expected) {
+				t.Errorf("result %v not equal to expected %v", res, test.expected)
+				return
+			}
+			for i := 0; i < len(res); i++ {
+				if res[i] != test.expected[i] {
+					t.Errorf("result %v not equal to expected %v", res, test.expected)
+					return
+				}
+			}
+		})
+	}
+}
+
+func TestFromBase10WithError(t *testing.T) {
+	type testCase struct {
+		num  uint64
+		base uint64
+	}
+
+	tests := map[string]testCase{
+		"target base 0": {
+			num:  1,
+			base: 0,
+		},
+		"target base 1": {
+			num:  0,
+			base: 1,
+		},
+	}
+
+	for name, test := range tests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			test := test
+			_, err := FromBase10(test.num, test.base)
+			if err == nil {
+				t.Fatal("expected non nil error")
+			}
+		})
+	}
+}
 
 func TestToBase10(t *testing.T) {
 	type testCase struct {
@@ -62,13 +161,14 @@ func TestToBase10(t *testing.T) {
 	}
 
 	for name, test := range tests {
+		test := test
 		t.Run(name, func(t *testing.T) {
-			val, err := ToBase10(test.num, test.base)
+			res, err := ToBase10(test.num, test.base)
 			if err != nil {
-				t.Fatalf("test case \"%s\": non nil error: %s", name, err)
+				t.Fatalf("unexpected non nil error: %v", err)
 			}
-			if val != test.expected {
-				t.Fatalf("test case \"%s\": expected %d not equal to actual %d", name, test.expected, val)
+			if res != test.expected {
+				t.Errorf("result %d not equal to expected %d", res, test.expected)
 			}
 		})
 	}
@@ -100,106 +200,17 @@ func TestToBase10WithError(t *testing.T) {
 	}
 
 	for name, test := range tests {
+		test := test
 		t.Run(name, func(t *testing.T) {
 			_, err := ToBase10(test.num, test.base)
 			if err == nil {
-				t.Fatalf("test case \"%s\": expected non nil error", name)
+				t.Fatal("expected non nil error")
 			}
 		})
 	}
 }
 
-func TestFromBase10(t *testing.T) {
-	type testCase struct {
-		num      uint64
-		base     uint64
-		expected []uint64
-	}
-
-	tests := map[string]testCase{
-		"convert zero": {
-			num:      0,
-			base:     2,
-			expected: []uint64{0},
-		},
-		"convert to binary": {
-			num:      5,
-			base:     2,
-			expected: []uint64{1, 0, 1},
-		},
-		"convert to binary with trailing zeros": {
-			num:      12,
-			base:     2,
-			expected: []uint64{1, 1, 0, 0},
-		},
-		"convert to base 62 with number less than 62": {
-			num:      5,
-			base:     62,
-			expected: []uint64{5},
-		},
-		"convert to base 62 with number equal to 62": {
-			num:      62,
-			base:     62,
-			expected: []uint64{1, 0},
-		},
-		"convert to base 62 with number larger than 62": {
-			num:      3_520_000_000_000,
-			base:     62,
-			expected: []uint64{61, 60, 14, 45, 17, 10, 24},
-		},
-		"convert to base 10 from base 10": {
-			num:      587,
-			base:     10,
-			expected: []uint64{5, 8, 7},
-		},
-		"convert to base 10 from base 10 with multiple of 10": {
-			num:      1000,
-			base:     10,
-			expected: []uint64{1, 0, 0, 0},
-		},
-	}
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			val, err := FromBase10(test.num, test.base)
-			if err != nil {
-				t.Fatalf("test case \"%s\": non nil error: %s", name, err)
-			}
-			if !reflect.DeepEqual(val, test.expected) {
-				t.Fatalf("test case \"%s\": expected %v not equal to actual %v", name, test.expected, val)
-			}
-		})
-	}
-}
-
-func TestFromBase10WithError(t *testing.T) {
-	type testCase struct {
-		num  uint64
-		base uint64
-	}
-
-	tests := map[string]testCase{
-		"target base 0": {
-			num:  1,
-			base: 0,
-		},
-		"target base 1": {
-			num:  0,
-			base: 1,
-		},
-	}
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			_, err := FromBase10(test.num, test.base)
-			if err == nil {
-				t.Fatalf("test case \"%s\": expected non nil error", name)
-			}
-		})
-	}
-}
-
-func TestGetLargestBase10Number(t *testing.T) {
+func TestGetLargestBase10(t *testing.T) {
 	type testCase struct {
 		base     uint64
 		digits   uint64
@@ -245,19 +256,20 @@ func TestGetLargestBase10Number(t *testing.T) {
 	}
 
 	for name, test := range tests {
+		test := test
 		t.Run(name, func(t *testing.T) {
-			val, err := GetLargestBase10Number(test.base, test.digits)
+			res, err := GetLargestBase10(test.base, test.digits)
 			if err != nil {
-				t.Fatalf("test case \"%s\": non nil error: %s", name, err)
+				t.Fatalf("unexpected non nil error: %v", err)
 			}
-			if val != test.expected {
-				t.Fatalf("test case \"%s\": expected %d not equal to actual %d", name, test.expected, val)
+			if res != test.expected {
+				t.Errorf("result %d not equal to expected %d", res, test.expected)
 			}
 		})
 	}
 }
 
-func TestGetLargestBase10NumberWithError(t *testing.T) {
+func TestGetLargestBase10WithError(t *testing.T) {
 	type testCase struct {
 		base   uint64
 		digits uint64
@@ -275,10 +287,11 @@ func TestGetLargestBase10NumberWithError(t *testing.T) {
 	}
 
 	for name, test := range tests {
+		test := test
 		t.Run(name, func(t *testing.T) {
-			_, err := GetLargestBase10Number(test.base, test.digits)
+			_, err := GetLargestBase10(test.base, test.digits)
 			if err == nil {
-				t.Fatalf("test case \"%s\": expected non nil error", name)
+				t.Fatal("expected non nil error")
 			}
 		})
 	}
@@ -342,10 +355,11 @@ func TestGetNumDigits(t *testing.T) {
 	}
 
 	for name, test := range tests {
+		test := test
 		t.Run(name, func(t *testing.T) {
-			numDigits := getNumDigits(test.num, test.base, test.tol)
-			if numDigits != test.expected {
-				t.Fatalf("test case \"%s\": expected %d not equal to actual %d", name, test.expected, numDigits)
+			res := getNumDigits(test.num, test.base, test.tol)
+			if res != test.expected {
+				t.Errorf("result %d not equal to expected %d", res, test.expected)
 			}
 		})
 	}
